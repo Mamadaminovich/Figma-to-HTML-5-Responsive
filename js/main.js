d20a7d21df32a0be92c630cd770a67d271a53e4b
Abdulaziz <mamadaminov001@gmail.com>
2022-09-06 18:29:08 +0500

    initial commit 55

--- js/main.js
+++ js/main.js
@@ -20,78 +20,10 @@ $(function () {
       }
     });
   }
-
   const menu = document.querySelector(".menu__list");
   const burger = document.querySelector(".menu__btn");
   burger.onclick = () => {
     menu.classList.toggle("active");
     burger.classList.toggle("active");
   };
-  // $(".testimon__slider").slick({
-  //   slidesToShow: 4,
-  //   slidesToScroll: 1,
-  //   infinity: true,
-  //   prevArrow:
-  //     '<button class="prev__btn slider__btn"><img src="images/prev__btn.svg"></button>',
-  //   nextArrow:
-  //     '<button class="next__btn slider__btn"><img src="images/next__btn.svg"></button>',
-  //   responsive: [
-  //     {
-  //       breakpoint: 1315,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 3,
-  //         infinite: true,
-  //         dots: false,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 780,
-  //       settings: {
-  //         slidesToShow: 2,
-  //         slidesToScroll: 2,
-  //         infinite: true,
-  //         dots: false,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 500,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1,
-  //       },
-  //     },
-  //   ],
-  // });
-  // $(".blog__inner").slick({
-  //   centerMode: true,
-  //   centerPadding: "0px",
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   infinity: true,
-  //   prevArrow:
-  //     '<button class="prev__btn-2 slider__btn"><img src="images/prev__btn.svg"></button>',
-  //   nextArrow:
-  //     '<button class="next__btn-2 slider__btn"><img src="images/next__btn.svg"></button>',
-  //   responsive: [
-  //     {
-  //       breakpoint: 800,
-  //       settings: {
-  //         slidesToShow: 2,
-  //         slidesToScroll: 1,
-  //         centerMode: false,
-  //         centerPadding: "0px",
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 540,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1,
-  //         centerMode: false,
-  //         centerPadding: "0px",
-  //       },
-  //     },
-  //   ],
-  // });
 });
